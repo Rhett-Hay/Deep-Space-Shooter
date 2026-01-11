17101565c114bdd86ee5c82b315a6c3ca5bbe85b
Rhett Haynes <rhetthay66@gmail.com>
2026-01-11 03:47:56 -0500

    Created animation movement for the robot.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,10 +1,12 @@
 using System;
 using StarterAssets;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] GameObject _hitVFXPrefab;
     [SerializeField] Animator _animator;
     [SerializeField] ParticleSystem _muzzleFlash;
     [SerializeField] int _damageAmount = 1;
@@ -36,10 +38,13 @@ public class Weapon : MonoBehaviour
 
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
+            string objectName = hit.collider.gameObject.name;
+
+            Instantiate(_hitVFXPrefab, hit.point, Quaternion.identity);
+            Debug.Log(objectName);
+
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(_damageAmount);
-                
-            //_starterAssetsInputs.ShootInput(false);
         }
     }
 }
